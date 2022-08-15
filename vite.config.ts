651c6f868906734df2fd0a3999da205c01243dd2
Zheheng Zhang <76390451+zhangzheheng12345@users.noreply.github.com>
2022-08-15 17:37:39 +0800

    Change base for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
-  base: "/birthday-count-downer/",
+  base: "",
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
